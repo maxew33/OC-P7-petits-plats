0fa449075109431b8196e5c9354a52711b93b1d0
Maxime Malfilâtre <maxime.malfilatre@gmail.com>
2023-02-08 11:13:35 +0100

    filtering by name / ingredients / description bubble sort done, display can be improved

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -4,22 +4,37 @@ import RecipeCard from "../templates/RecipeCard.js"
 import TagList from "../templates/TagList.js"
 
 const api = new Api('data/recipes.json'),
+    recipesInfos = [],// all recipes
+    tempRecipesInfos = [],// recipes temp array
     $recipesContainer = document.querySelector('.recipes'),
     $tagsList = Array.from(document.querySelectorAll('.tags__list')),
+    $searchInput = document.querySelector('#search__text'),
     ingredientsArray = [],
     applianceArray = [],
     ustensilsArray = [],
     descriptionArray = [],
-    tagsArray = [{ingredients : ingredientsArray}, {appliance: applianceArray}, {ustensils: ustensilsArray}]
+    tagsArray = [{ ingredients: ingredientsArray }, { appliance: applianceArray }, { ustensils: ustensilsArray }]
 
-async function main() {
+let searchWord = ''
 
-    const recipesInfos = await api.getRecipes()
+async function getRecipes() {
+
+    /* Getting the recipes from the API and pushing them into the recipesInfos and the temp recipes array. */
+    const recipes = await api.getRecipes()
+    recipesInfos.push(...Array.from(recipes, x => x))
+    tempRecipesInfos.push(...Array.from(recipes, x => x))
+
+    fillRecipes(tempRecipesInfos)
+}
+
+const fillRecipes = recipes => {
+
+    $recipesContainer.innerHTML = ''
 
     // todo : essayer de sortir l'appendchild de la boucle
+    // essayer de ne pas refaire des appendchild à chaque fois mais plutôt utiliser des dataset id pour display ou non les recettes
 
-    recipesInfos
-        .map(recipe => new Recipe(recipe))
+    recipes.map(recipe => new Recipe(recipe))
         .forEach(recipe => {
             const card = new RecipeCard(recipe)
             const myCard = card.createRecipeCard()
@@ -37,7 +52,7 @@ async function main() {
         $tagsList[idx].appendChild(myList)
     })
 
-console.log(applianceArray)
+    console.log(applianceArray)
 }
 
 const fillArrays = (arrayValues, arrayToFill) => {
@@ -45,4 +60,44 @@ const fillArrays = (arrayValues, arrayToFill) => {
     console.log()
 }
 
-main()
+$searchInput.addEventListener('input', () => {
+    searchWord = $searchInput.value.length > 2 ? $searchInput.value : ''
+    console.log(searchWord)
+    searchWord && filteringRecipes(searchWord, tempRecipesInfos)
+})
+
+const filteringRecipes = (word, recipes) => {
+    console.log(word, recipes)
+
+    const searchedWord = word.toLowerCase()
+
+    const tempRecipes = []
+
+    recipes.forEach(recipe => {
+        const { name, ingredients, description } = recipe
+
+        let myIngredients = '',
+            myDescription = description.toLowerCase()
+
+        ingredients.forEach(elt => myIngredients += elt.ingredient.toLowerCase() + ' ')
+
+        if (name.toLowerCase().match(searchedWord)) {
+            console.log('name match')
+            tempRecipes.push(recipe)
+        }
+        else if (myIngredients.match(searchedWord)) {
+            console.log('ingredients match')
+            tempRecipes.push(recipe)
+        }
+        else if (myDescription.match(searchedWord)) {
+            console.log('description match')
+            tempRecipes.push(recipe)
+        }
+    }
+    )
+
+    fillRecipes(tempRecipes)
+
+}
+
+getRecipes()
