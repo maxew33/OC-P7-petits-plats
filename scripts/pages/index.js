eb5e72048ba108edaafb9e01a9a6d73fc885c6f0
Maxime Malfilâtre <maxime.malfilatre@gmail.com>
2023-02-12 10:07:30 +0100

    filtering with tags done, todo update when tags is removed

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -6,6 +6,8 @@ import TagList from "../templates/TagList.js"
 const api = new Api('data/recipes.json'),
     recipesInfos = [],// all recipes
     tempRecipesInfos = [],// recipes temp array
+    searchBarRecipes = [],
+    tagsRecipes = [],
     $recipesContainer = document.querySelector('.recipes'),
     $recipeCard = [],
     $tagsList = Array.from(document.querySelectorAll('.tags__list')),
@@ -14,15 +16,17 @@ const api = new Api('data/recipes.json'),
     ingredientsArray = [],
     applianceArray = [],
     ustensilsArray = [],
-    descriptionArray = [],
     tagsArray = [{ ingredients: ingredientsArray }, { appliance: applianceArray }, { ustensils: ustensilsArray }],
     recipesTags = [],
     tagsSelected = { ingredients: [], appliance: [], ustensils: [] },
     $tagsOpener = Array.from(document.querySelectorAll('.tags__opener')),
-    $tagSelectedContainer = document.querySelector('.tags__selected')
+    $tagSelectedContainer = document.querySelector('.tags__selected'),
+    tagsRecipesIdArray = {}
 
-let searchWord = '',
-    filtered = false // if the recipes have already been filtered
+let filtered = false // if the recipes have already been filtered
+
+
+// GET RECIPES
 
 async function getRecipes() {
 
@@ -31,16 +35,19 @@ async function getRecipes() {
     recipesInfos.push(...Array.from(recipes, x => x))
     tempRecipesInfos.push(...Array.from(recipes, x => x))
 
-    fillRecipes(tempRecipesInfos)
+    fillPage(tempRecipesInfos)
 }
 
-const fillRecipes = recipes => {
+// CREATE ALL THE DOM ELEMENTS (REICPES AND TAGS)
+
+const fillPage = recipes => {
 
     $recipesContainer.innerHTML = ''
 
     // todo : essayer de sortir l'appendchild de la boucle
     // essayer de ne pas refaire des appendchild à chaque fois mais plutôt utiliser des dataset id pour display ou non les recettes
 
+    //create the card for the recipes
     recipes.map(recipe => new Recipe(recipe))
         .forEach(recipe => {
             const card = new RecipeCard(recipe)
@@ -48,12 +55,15 @@ const fillRecipes = recipes => {
 
             $recipesContainer.appendChild(myCard)
 
-            const myArrays = [[recipe.ingredientsArray, ingredientsArray], [recipe.descriptionArray, descriptionArray], [recipe.ustensilsArray, ustensilsArray], [recipe.applianceArray, applianceArray]]
-            myArrays.forEach(array => fillArrays(array[0], array[1]))
+            const myArrays = [[recipe.ingredientsArray, ingredientsArray], [recipe.ustensilsArray, ustensilsArray], [recipe.applianceArray, applianceArray]]
+            myArrays.forEach(array => fillArrays(array[0], array[1], recipe.id))
 
             const recipeTags = { id: recipe.id, ingredients: recipe.ingredientsArray, ustensils: recipe.ustensilsArray, appliance: recipe.applianceArray }
             recipesTags.push(recipeTags)
         })
+
+    //fill my recipe cards array with all the created cards
+    Array.from(document.querySelectorAll('.card')).forEach(elt => { $recipeCard.push(elt) })
 
     //create the tags and fill the tags container
     tagsArray.forEach((elt, idx) => {
@@ -62,24 +72,37 @@ const fillRecipes = recipes => {
         $tagsList[idx].appendChild(myList)
     })
 
-    //fill my recipe cards array with all the created cards
-    Array.from(document.querySelectorAll('.card')).forEach(elt => { $recipeCard.push(elt) })
+
     //fill my tags buttons array with all the created tags    
-    // Array.from(document.querySelectorAll('.tag__btn')).forEach(elt => { $tagsBtn.push(elt) })
     $tagsList.forEach(list => $tagsBtn.push(list.querySelectorAll('.tag__btn')))
     $tagsBtn.forEach(btnList => btnList.forEach(btn => btn.addEventListener('click', e => tagClicked(e))))
 }
 
-const displayCards = recipes => {
+// fill the 
+
+const fillArrays = (arrayValues, arrayToFill, id) => {
+ 
+    arrayValues.forEach(elt => {
+        tagsRecipesIdArray[elt] ? tagsRecipesIdArray[elt].push(id) : tagsRecipesIdArray[elt]=[id]
+
+        arrayToFill.indexOf(elt) === -1 && arrayToFill.push(elt)})
+}
+
+// UPDATE THE PAGE WITH THE FILTERED DATA (RECIPE / TAGS)
+
+const updatePage = recipes => {
+
+    // update the recipes cards
+
     //creation of the id array
     const idArray = []
 
     recipes.forEach(recipe => idArray.push(recipe.id))
 
     $recipeCard.forEach(card => card.style.display = idArray.includes(+card.dataset.id) ? 'block' : 'none')
-}
 
-const displayTags = recipes => {
+
+    // update the recipes tags
 
     //clear the tags array
     tagsArray.forEach(tag => Object.values(tag)[0].length = 0)
@@ -93,6 +116,7 @@ const displayTags = recipes => {
         myArrays.forEach(array => fillArrays(array[0], array[1]))
     })
 
+    //display the tags
     tagsArray.forEach((tag, idx) => {
         const myListData = Object.values(tag)[0]
         const valueArray = []
@@ -102,13 +126,11 @@ const displayTags = recipes => {
     })
 }
 
-const fillArrays = (arrayValues, arrayToFill) => {
-    arrayValues.forEach(elt => arrayToFill.indexOf(elt) === -1 && arrayToFill.push(elt))
-}
+// SEARCH A RECIPE
+// with search bar
 
 $searchInput.addEventListener('input', () => {
-    searchWord = $searchInput.value.length > 2 ? $searchInput.value : filtered && (filtered = !filtered, displayCards(recipesInfos), displayTags(recipesInfos))
-    searchWord && filteringRecipes(searchWord, tempRecipesInfos)
+    $searchInput.value.length > 2 ? filteringRecipes($searchInput.value, tempRecipesInfos) : filtered && (filtered = !filtered, updatePage(recipesInfos))
 })
 
 const filteringRecipes = (word, recipes) => {
@@ -117,7 +139,7 @@ const filteringRecipes = (word, recipes) => {
 
     const searchedWord = word.toLowerCase()
 
-    const tempRecipes = []
+    searchBarRecipes.length = 0
 
     recipes.forEach(recipe => {
         const { name, ingredients, description } = recipe
@@ -129,24 +151,30 @@ const filteringRecipes = (word, recipes) => {
         ingredients.forEach(elt => myIngredients += elt.ingredient.toLowerCase() + ' ')
 
         if (myName.match(searchedWord)) {
-            tempRecipes.push(recipe)
+            searchBarRecipes.push(recipe)
         }
         else if (myIngredients.match(searchedWord)) {
-            tempRecipes.push(recipe)
+            searchBarRecipes.push(recipe)
         }
         else if (myDescription.match(searchedWord)) {
-            tempRecipes.push(recipe)
+            searchBarRecipes.push(recipe)
         }
     }
     )
 
-    // fillRecipes(tempRecipes)
-    displayCards(tempRecipes)
-    displayTags(tempRecipes)
+
+    // fillPage(tempRecipes)
+    updatePage(searchBarRecipes)
+    // displayTags(tempRecipes)
 }
+
+
+// TAGS SEARCH
 
 const tagClicked = e => {
     // tagsSelected.Object.keys(e.target.parentElement.className).push(e.target.dataset.value)
+
+//set the style of the clicked tag and add it in the tags container
     Object.keys(tagsSelected).forEach(tagCategory => {
         if (tagCategory === e.target.parentElement.className) {
             tagsSelected[tagCategory].push(e.target.dataset.value)
@@ -154,6 +182,18 @@ const tagClicked = e => {
             createTagSelectedElement(e.target.dataset.value, tagCategory)
         }
     })
+
+    tagsRecipes.length === 0 && (tagsRecipes.push(...recipesInfos))
+
+    const tempRecipesList = []
+
+    tagsRecipesIdArray[e.target.dataset.value].forEach(id => tagsRecipes.forEach(recipe => recipe.id === id && tempRecipesList.push(recipe)))
+
+
+    tagsRecipes.length = 0
+    tagsRecipes.push(...tempRecipesList)
+
+    updatePage(tagsRecipes)
 
     /*
     1- ajouter le tag dans .tags__selected au click
